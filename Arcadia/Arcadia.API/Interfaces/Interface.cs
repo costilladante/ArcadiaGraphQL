91a68d06a00ccf0de074a5d3447b11b18ca00745
Costilla Dante <costilladante@github.com>
2017-09-27 19:44:23 -0300

    Add HeroInterface

---
+++ Arcadia/Arcadia.API/Interfaces/Interface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Arcadia.API.Models;
+
+namespace Arcadia.API.Interfaces
+{
+    public interface IHeroRepository
+    {
+        Task<Hero> Get(int id);
+    }
+}
